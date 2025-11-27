9272c8a8fedef10b8a54bad4ffc18b1c159d24d6
Satish Gupta <gsatish.june@gmail.com>
2025-11-27 19:35:13 +0530

    Update setup_disc info.sh

--- setup_disc info.sh
+++ setup_disc info.sh
@@ -8,7 +8,7 @@ cp -n "$FILE" "$BACKUP"
 
 echo "==> Injecting rocky9.6 discinfo entry..."
 
-sed -i '/"1699827798\.672747" *=> *"rocky9\.3"/i\    "1748309243.9338255" => "rocky9.8",      #x86_64' "$FILE"
+sed -i '/"1699827798\.672747" *=> *"rocky9\.3"/i\    "1748309243.9338255" => "rocky9.6",      #x86_64' "$FILE"
 
 echo "==> Showing updated section:"
 grep -n 'rocky9' "$FILE"
